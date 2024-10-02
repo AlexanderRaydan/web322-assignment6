dd03deb0a6e56bbb9de264d5fe9b1a99b3f6fb92
Alexander Raydan <alex@Alexanders-MacBook-Air.local>
2024-10-02 12:16:20 -0400

    initial commit

---
+++ module/legoSets.js
@@ -0,0 +1,55 @@
+/*********************************************************************************
+* WEB322 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Alexander Raydan Student ID: 124348236 Date: Sep 4 2024
+********************************************************************************/
+
+const setData = require("../data/setData");
+const themeData = require("../data/themeData");
+
+let sets = [];
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        let theme;
+        setData.forEach(lego => {
+            theme = themeData.find(e => e.id === lego.theme_id)
+            lego.theme = theme.name;
+            sets.push(lego);
+        });
+        sets.length !== 0 ? resolve('Data initialized successfully') : reject('No data')
+    });
+}
+
+function getAllSets() {
+
+    return new Promise((resolve, reject) => {
+        initialize().then(
+            sets.length !== 0 ? resolve(sets) : reject('No data')
+        )
+    });
+}
+
+function getSetByNum(setNum) {
+    let set = sets.find(e => e.set_num === setNum)
+    return new Promise((resolve, reject) => {
+        initialize().then(
+            set !== undefined ? resolve(set) : reject(`Set number ${setNum} not found`)
+        )
+    });
+}
+
+
+function getSetsByTheme(theme) {
+    let set = sets.filter(e => e.theme.toUpperCase().includes(theme.toUpperCase()))
+    return new Promise((resolve, reject) => {
+        initialize().then(
+            set.length !== 0 ? resolve(set) : reject(`Set theme ${theme} not found`)
+        )
+    });
+}
+
+module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme }
---
+++ server.js
@@ -0,0 +1,96 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Alexander Raydan Student ID: 124348236 Date: Sep 4 2024
+********************************************************************************/
+
+const express = require('express'); // "require" the Express module
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const path = require('path');
+
+const legoSets = require("./module/legoSets");
+legoSets.initialize()
+
+// start the server on the port and output a confirmation to the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
+app.use(express.static('public'));
+
+// Home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+
+// About page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/lego/sets', (req, res) => {
+    const theme = req.query.theme; // Check for the 'theme' query parameter
+
+    if (theme) {
+        // If 'theme' parameter exists, fetch sets by theme
+        legoSets.getSetsByTheme(theme)
+            .then(
+                (sets) => res.send(sets)
+            )
+            .catch(
+                (err) => res.status(404).send({ error: err.message })
+            );
+    } else {
+        // If 'theme' parameter doesn't exist, return all sets
+        legoSets.getAllSets()
+            .then(
+                (sets) => res.send(sets)
+            )
+            .catch(
+                (err) => res.status(404).send({ error: err.message })
+            );
+    }
+});
+
+app.get('/lego/sets/:set_num', (req, res) => {
+    const setNum = req.params.set_num;
+    legoSets.getSetByNum(setNum)
+        .then(
+            (e) => res.send(e)
+        )
+        .catch(
+            (err) => res.status(404).send({ error: err.message })
+        );
+});
+
+
+app.get('/lego/sets/bad-num-demo', (req, res) => {
+    legoSets.getSetByNum('bad_id')
+        .then(
+            (e) => res.send(e)
+        )
+        .catch(
+            (err) => res.send(err)
+        )
+});
+
+
+app.get('/lego/sets/bad-theme-demo', (req, res) => {
+    legoSets.getSetsByTheme('bad_id')
+        .then(
+            (e) => res.send(e)
+        )
+        .catch(
+            (err) => res.send(err)
+        )
+});
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
